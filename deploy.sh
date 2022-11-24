9476deb3b9b0a8f947bce6dc827e9756d13a4301
Queku Bonty <ryanbonty@hotmail.com>
2022-11-24 17:18:58 +0000

    added contents to deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# this is a deployment script
+#had to make a few changes to your script, LOLA
+#there was a typo indeploy
+
+mkdir deploy
+touch deploy/java.app
+echo  "life changing destiny" > deploy/java.app
+echo "welcome to G19"
+echo "please enter your name
+read $name
+echo "thank you $name"
